a64a828f77bedaf069289077acc97b242aee918b
pawansoft <46833870+pawansoft@users.noreply.github.com>
2020-10-27 18:19:17 +0530

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -1,8 +1,5 @@
 public class Driver {
     public static void main(String[] args) {
-     AddressBookPojo newAddressBookPojo = new AddressBookPojo();
-     AddressBookDAO addressBookDAO=new AddressBookDAO();
-     newAddressBookPojo=addressBookDAO.askingForDetail();
-        System.out.println(addressBookDAO.saveDetails(newAddressBookPojo.getId(), newAddressBookPojo));
+        System.out.println("Wlecome to Address Book Dao");
     }
 }
