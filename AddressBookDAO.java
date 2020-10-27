6375f969724eaeddeb8d47ce83e56e22e9a4be0e
Pawan Kumar <pk.soft29@gmail.com>
2020-10-27 09:35:34 +0530

    Added : functionality to add new Address

---
+++ AddressBookDAO.java
@@ -0,0 +1,55 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class AddressBookDAO {
+    HashMap <String , AddressBookPojo> addressContainer=new HashMap<String, AddressBookPojo>();
+    Scanner sc=new Scanner(System.in);
+    public HashMap<String, AddressBookPojo> saveDetails (String id, AddressBookPojo addressBookPojo)
+    {
+        try {
+            addressContainer.put(id, addressBookPojo);
+            return addressContainer;
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
+
+
+    public AddressBookPojo askingForDetail()
+    {
+        AddressBookPojo newAddressBookPojo=new AddressBookPojo();
+        System.out.println("Enter the Id: ");
+        String id=sc.next();
+        System.out.println("Enter the fName");
+        String fName=sc.next();
+        System.out.println("Enter the lName");
+        String lName=sc.next();
+        System.out.println("Enter The Address");
+        String address=sc.next();
+        System.out.println("Enter the city");
+        String city=sc.next();
+        System.out.println("Enter the state");
+        String state=sc.next();
+        System.out.println("Enter the zip Code");
+        int zip=sc.nextInt();
+        System.out.println("Enter the phone number");
+        long phone = sc.nextLong();
+        System.out.println("Enter your email Id");
+        String email=sc.next();
+        newAddressBookPojo.setId(id);
+        newAddressBookPojo.setfName(fName);
+        newAddressBookPojo.setlName(lName);
+        newAddressBookPojo.setAddress(address);
+        newAddressBookPojo.setCity(city);
+        newAddressBookPojo.setState(state);
+        newAddressBookPojo.setZip(zip);
+        newAddressBookPojo.setPhone(phone);
+        newAddressBookPojo.setEmail(email);
+        return newAddressBookPojo;
+    }
+
+}
