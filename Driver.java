3c6986474d19f563fb49cfac6c99251dc10ac11c
pawansoft <46833870+pawansoft@users.noreply.github.com>
2020-10-28 08:49:59 +0530

    added: Delete function to delete address

--- Driver.java
+++ Driver.java
@@ -5,27 +5,19 @@ public class Driver {
         Scanner sc=new Scanner(System.in);
      AddressBookPojo newAddressBookPojo = new AddressBookPojo();
      AddressBookDAO addressBookDAO=new AddressBookDAO();
-
-
-        boolean isTerminate = false;
-        while (isTerminate != true)
-        {
-            System.out.println("Enter 1 for add New Address, Enter 2 for update Address,4 : delete Address, Enter 3 for terminate the program");
+     newAddressBookPojo=addressBookDAO.askingForDetail();
+     System.out.println(addressBookDAO.saveDetails(newAddressBookPojo.getId(), newAddressBookPojo));  
+   
+            System.out.println(" 2:  for update Address,4 : delete Address");
             int option=sc.nextInt();
             switch (option){
-                case 1:
-                    newAddressBookPojo=addressBookDAO.askingForDetail();
-                    System.out.println(addressBookDAO.saveDetails(newAddressBookPojo.getId(), newAddressBookPojo));
-                    break;
                 case 2:
                     System.out.println("Enter an Id to update: ");
                     String id=sc.next();
                     newAddressBookPojo=addressBookDAO.askingForDetail();
                     System.out.println(addressBookDAO.updateAddressBook(id, newAddressBookPojo));
                     break;
-                case 3:
-                    isTerminate=true;
-                    break;
+
                 case 4:
                     System.out.println("Enter the Id to delete");
                     String key=sc.next();
@@ -35,9 +27,6 @@ public class Driver {
                     System.out.println("Please provide valid option");
                     break;
             }
-        }
-
-
         }
 }
 
